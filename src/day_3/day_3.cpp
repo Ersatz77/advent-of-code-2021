8da65fb8220a20767d1710a26dade92b5af13113
Ersatz <cm12mc@gmail.com>
2021-12-03 01:26:02 -0800

    Simplified solution for day 3

--- src/day_3/day_3.cpp
+++ src/day_3/day_3.cpp
@@ -15,6 +15,12 @@ namespace aoc
 {
 	constexpr int num_bits = 12;
 
+	enum class Life_support_type
+	{
+		OXYGEN_GENERATOR = 0,
+		CO2_SCRUBBER
+	};
+
 	std::vector<std::bitset<num_bits>> parse_input(const std::filesystem::path& path)
 	{
 		std::ifstream file = open_file(path);
@@ -27,7 +33,7 @@ namespace aoc
 		return binary_numbers;
 	}
 
-	std::bitset<num_bits> oxygen_generator_rating(const std::vector<std::bitset<num_bits>>& binary_numbers)
+	std::bitset<num_bits> life_support_rating(const std::vector<std::bitset<num_bits>>& binary_numbers, const Life_support_type life_support_type)
 	{
 		std::vector<std::bitset<num_bits>> valid_numbers = binary_numbers;
 		for (size_t i = 0; i < num_bits; ++i)
@@ -44,42 +50,25 @@ namespace aoc
 				[offset](const auto& bin) { return bin[offset] == 0; });
 			size_t num_ones = std::count_if(valid_numbers.begin(), valid_numbers.end(), 
 				[offset](const auto& bin) { return bin[offset] == 1; });
-			int most_common = num_zeros <= num_ones ? 1 : 0;
+
+			int common = 0;
+			switch (life_support_type)
+			{
+			case Life_support_type::OXYGEN_GENERATOR:
+				// Most common
+				common = num_zeros <= num_ones ? 1 : 0;
+				break;
+
+			case Life_support_type::CO2_SCRUBBER:
+				// Least common
+				common = num_zeros <= num_ones ? 0 : 1;
+				break;
+			}
 
 			// Copy all new valid numbers to a temp vector then override the vector with previously valid numbers
 			std::vector<std::bitset<num_bits>> temp_valid_numbers;
 			std::copy_if(valid_numbers.begin(), valid_numbers.end(), std::back_inserter(temp_valid_numbers),
-				[offset, most_common](const auto& bin) { return static_cast<int>(bin[offset]) == most_common; });
-
-			valid_numbers = temp_valid_numbers;
-		}
-
-		return valid_numbers.front();
-	}
-
-	std::bitset<num_bits> co2_scrubber_rating(const std::vector<std::bitset<num_bits>>& binary_numbers)
-	{
-		std::vector<std::bitset<num_bits>> valid_numbers = binary_numbers;
-		for (size_t i = 0; i < num_bits; ++i)
-		{
-			// Break out of loop if one number is left
-			if (valid_numbers.size() == 1)
-				break;
-
-			// Create an offset because indexing position 0 of a 'std::bitset' is the most significant bit and we need the least significant first
-			size_t offset = (num_bits - 1) - i;
-
-			// Find the least common bit
-			size_t num_zeros = std::count_if(valid_numbers.begin(), valid_numbers.end(), 
-				[offset](const auto& bin) { return bin[offset] == 0; });
-			size_t num_ones = std::count_if(valid_numbers.begin(), valid_numbers.end(), 
-				[offset](const auto& bin) { return bin[offset] == 1; });
-			int least_common = num_zeros <= num_ones ? 0 : 1;
-
-			// Copy all new valid numbers to a temp vector then override the vector with previously valid numbers 
-			std::vector<std::bitset<num_bits>> temp_valid_numbers;
-			std::copy_if(valid_numbers.begin(), valid_numbers.end(), std::back_inserter(temp_valid_numbers),
-				[offset, least_common](const auto& bin) { return static_cast<int>(bin[offset]) == least_common; });
+				[offset, common](const auto& bin) { return static_cast<int>(bin[offset]) == common; });
 
 			valid_numbers = temp_valid_numbers;
 		}
@@ -108,8 +97,8 @@ namespace aoc
 	const std::string Day_3::part_2(const std::filesystem::path& input_path) const
 	{
 		std::vector<std::bitset<num_bits>> numbers = parse_input(input_path / "day_3.txt");
-		std::bitset<num_bits> oxygen_rating = oxygen_generator_rating(numbers);
-		std::bitset<num_bits> co2_rating = co2_scrubber_rating(numbers);
+		std::bitset<num_bits> oxygen_rating = life_support_rating(numbers, Life_support_type::OXYGEN_GENERATOR);
+		std::bitset<num_bits> co2_rating = life_support_rating(numbers, Life_support_type::CO2_SCRUBBER);
 
 		return fmt::format("Day 3 Part 2 | Life support rating: {}", oxygen_rating.to_ullong() * co2_rating.to_ullong());
 	}
